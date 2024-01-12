d39f0a33d81942321f04361a374636ca54c3c9f3
abdulfayis681 <103239046+abdulfayis681@users.noreply.github.com>
2024-01-12 17:48:31 +0530

    Update main.tf

--- modules/gke/main.tf
+++ modules/gke/main.tf
@@ -30,6 +30,7 @@ resource "google_container_cluster" "primary" {
   }
   master_authorized_networks_config {
     cidr_blocks = ["0.0.0.0/0"]
+  }
 
   workload_identity_config {
     workload_pool = "${var.projectid}.svc.id.goog"
