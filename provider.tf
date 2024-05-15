0b3d8261bf1fc3cf0a862da76f09d1d9a9c6081d
rohitniveus <126222291+rohitniveus@users.noreply.github.com>
2024-05-15 08:55:24 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -12,17 +12,18 @@ terraform {
       version = "4.0"
     }
   }
-
+/*
   # Store Terraform state and the history of all revisions remotely, and protect that state with locks to prevent corruption.
   backend "gcs" {
     bucket      = "f4yiee9744"
     prefix      = "terraform/state"
     credentials = "./cred.json"
   }
+*/
 }
 
 provider "google" {
-  credentials = file("./cred.json")
+  #credentials = file("./cred.json")
   project     = var.projectid
   region      = var.region
   # version = "~> 2.18"
